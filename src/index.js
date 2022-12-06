70a2ebc3f0297ed73478cd71cbff45bc24080bf8
IrPanTech <irfannkamill@gmail.com>
2022-12-06 21:33:42 +0700

    asjhd

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./index.css";
-// DEFVELOP
+// ASJHD
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
