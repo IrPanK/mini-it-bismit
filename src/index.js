a755848e910f131b633242d004fdec5cfefa941a
IrPanTech <irfannkamill@gmail.com>
2022-12-06 21:28:05 +0700

    vuba

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./index.css";
-<<<<<<< HEAD
-// samiasnansn
-// develop
-=======
-// INI DEVCELOP BARU YG BENER
->>>>>>> develop
+// MEnvoBENER
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
