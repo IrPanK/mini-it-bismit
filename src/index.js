4cd4c8bb867e7d064418ae4b84381da017d77d89
IrPanTech <irfannkamill@gmail.com>
2022-12-06 22:21:17 +0700

    finish project

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./index.css";
-<<<<<<< HEAD
-// asdjakjas
-// sakdjajs
-// kolal
-=======
 
-// kakaka
-// madimadaja
-// misakla
-// asdakakak
-
->>>>>>> madi
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
