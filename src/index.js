811ff99f10a1f1627782fd0d387ea00b181cd74e
IrPanTech <irfannkamill@gmail.com>
2022-12-06 21:24:58 +0700

    develop uy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./index.css";
-// uwawa
+// INI DEVCELOP BARU YG BENER
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
